7724215bd4b0d9167eba29ee1b538fe1a7c9331e
Masheba <nikolaiageev1991@gmail.com>
2021-09-20 13:33:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 10000;
+        int bonus = 20;
+        int total = ticketprice / bonus;
+        System.out.println(total);
+    }
+
+
+}
